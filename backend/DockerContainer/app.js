88cdec5e6e3360d3c266e8b695e50c63fe383f9a
Erik Töger <erik.toger@gmail.com>
2024-05-10 08:07:53 +0200

    Trigger Docker pipeline

--- backend/DockerContainer/app.js
+++ backend/DockerContainer/app.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const port = 8080;
 
 router.get('/', function(_req,res){
-    res.send({message:"Hello from node in docker"});
+    res.send({message:"Hello from Node in Docker"});
   });
 
 app.use('/', router);
