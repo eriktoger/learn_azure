15a43587dae43e6727381ae41af0faead2b55f12
Erik Töger <erik.toger@gmail.com>
2024-05-10 10:39:36 +0200

    Trigger docker redeploy

--- backend/DockerContainer/app.js
+++ backend/DockerContainer/app.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const port = 8080;
 
 router.get('/', function(_req,res){
-    res.send({message: "Hello from Node in Docker"});
+    res.send({message: "Hello from Node in Docker!!"});
   });
 
 app.use('/', router);
